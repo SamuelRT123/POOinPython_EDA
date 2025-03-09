d232c3da241d085ce730558f863a6bdc22f9cae4
samuelrt123 <se.rodriguez2@uniandes.edu.co>
2025-03-09 10:17:49 -0500

    Ajuste clase

--- DataStructure/scht.py
+++ DataStructure/scht.py
@@ -1,20 +1,6 @@
-﻿"""
-Este ADT representa una estructura de datos lineal, específicamente un arreglo dinámico (**Arraylist**). El arreglo dinámico es una estructura de datos que permite almacenar un conjunto de elementos del mismo tipo, en la cual se puede acceder y procesar sus elementos utilizando las funciones propias de la estructura.
-
-*IMPORTANTE:* Este código y sus especificaciones para Python están basados en las implementaciones propuestas por los siguientes autores/libros:
-
-    #. Algorithms, 4th Edition, Robert Sedgewick y Kevin Wayne.
-    #. Data Structure and Algorithms in Python, M.T. Goodrich, R. Tamassia, M.H. Goldwasser.
-"""
-
-# native python modules
-# import dataclass to define the array list
-from dataclasses import dataclass, field
-# import modules for defining the element's type in the array
+﻿from dataclasses import dataclass, field
 from typing import List, Optional, Callable, Generic, TypeVar
 from linkedlist import SingleLinked as linkedlist
-# import inspect for getting the name of the current function
-import inspect
 
 T = TypeVar("T")
 
